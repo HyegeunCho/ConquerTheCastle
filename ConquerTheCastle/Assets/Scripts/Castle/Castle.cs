5f37a720429ff939b582a0463f5d44797c862e44
HyegeunCho <hgcho84.kr@gmail.com>
2022-06-22 01:13:52 +0900

    [추가] 성 타이머 추가

--- ConquerTheCastle/Assets/Scripts/Castle/Castle.cs
+++ ConquerTheCastle/Assets/Scripts/Castle/Castle.cs
@@ -1,11 +1,18 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using UniRx;
 using UnityEngine;
 
 public class Castle : MonoBehaviour
 {
+    [SerializeField] private TextMeshPro TXT_CastleHP;
+    
     [Range(1, 10)] public float Height;
+    public int MaxHp = 60;
+    
+    [HideInInspector] public ReactiveProperty<int> CurrentHp;
 
     public float CurrentHeight
     {
@@ -15,15 +22,48 @@ public class Castle : MonoBehaviour
             transform.localScale = new Vector3(1, value, 1);
         }
     }
+
+    private void Awake()
+    {
+        CurrentHp = new ReactiveProperty<int>(0);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        CastleTimer.Instance.Initialize();
+        CurrentHp.ObserveOnMainThread().Subscribe(OnUpdateCastleHp).AddTo(this);
+
+        CastleTimerManager.Instance.OnSeconds.Subscribe(OnTimerSeconds).AddTo(this);
+        // CastleTimerManager.Instance.OnHalf.Subscribe(OnTimerHalf).AddTo(this);
+        // CastleTimerManager.Instance.OnThird.Subscribe(OnTimerThird).AddTo(this);
+    }
+    
+    private void OnUpdateCastleHp(int inHp)
+    {
+        TXT_CastleHP.text = $"{CurrentHp}";
     }
 
+    private void OnTimerSeconds(long inCount)
+    {
+        RegenerateCastleHp();
+    }
 
-    // Update is called once per frame
-    
+    private void RegenerateCastleHp()
+    {
+        if (CurrentHp.Value >= MaxHp) return;
+        CurrentHp.Value = CurrentHp.Value + 1;
+    }
+
+    private void OnTimerHalf(int inCount)
+    {
+        
+    }
+
+    private void OnTimerThird(int inCount)
+    {
+        
+    }
+
     void Update()
     {
         UpdateHeight(Height);
